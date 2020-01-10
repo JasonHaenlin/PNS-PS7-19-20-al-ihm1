0056235c6cc469ee38df257c2512388101a7232e
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-01-10 16:35:58 +0100

    #4 Event vizualisation in agenda

--- polympic/lib/main.dart
+++ polympic/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:polympic/blocs/basic/category_bloc.dart';
+import 'package:polympic/blocs/basic/event-bloc/event_bloc.dart';
 import 'package:polympic/blocs/bloc_provider.dart';
 import 'package:polympic/blocs/loading/loading_bloc.dart';
 import 'package:polympic/screens/app-page/app-page.dart';
@@ -10,14 +11,17 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<LoadingBloc>(
-      bloc: LoadingBloc(),
-      child: BlocProvider<CategoryBloc>(
-        bloc: CategoryBloc(),
-        child: MaterialApp(
-          title: 'Polympic basic',
-          theme: appTheme(),
-          home: AppPage(),
+    return BlocProvider<EventBloc>(
+      bloc: EventBloc(),
+      child: BlocProvider<LoadingBloc>(
+        bloc: LoadingBloc(),
+        child: BlocProvider<CategoryBloc>(
+          bloc: CategoryBloc(),
+          child: MaterialApp(
+            title: 'Polympic basic',
+            theme: appTheme(),
+            home: AppPage(),
+          ),
         ),
       ),
     );
