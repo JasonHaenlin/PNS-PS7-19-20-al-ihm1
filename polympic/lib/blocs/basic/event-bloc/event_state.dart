87dcc661502fa0eddf9e324a3c695773fa82060e
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-01-10 16:29:24 +0100

    #12 block for events

---
+++ polympic/lib/blocs/basic/event-bloc/event_state.dart
@@ -0,0 +1,22 @@
+import 'package:polympic/blocs/bloc_base.dart';
+import 'package:polympic/models/event_model.dart';
+import 'package:polympic/services/event-service.dart';
+
+enum EventEvent { fetch, update }
+
+class EventState extends BlocBase<EventEvent, List<EventModel>> {
+  EventState() : super(initEvent: EventEvent.fetch);
+
+  @override
+  Stream<List<EventModel>> eventToState(EventEvent event, [data]) async* {
+    switch (event) {
+      case EventEvent.fetch:
+        yield await eventService.getData();
+        break;
+      case EventEvent.update:
+        eventService.saveChange(data['event'], data['favorited']);
+        break;
+      default:
+    }
+  }
+}
