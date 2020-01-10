5a76240a13f923e416ee719087286d6e57576ff2
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-01-10 16:26:30 +0100

    #28 Add a local events mock in front end

---
+++ polympic/lib/mocks/event_model.dart
@@ -0,0 +1,28 @@
+const EVENT_MOCK = [
+
+  {
+    "id" : 1,
+    "name" : "Match de Foot",
+    "description" : "Match d'ouverture",
+    "sport" : "Foot",
+    "place" : "Stade de France",
+    "versus" : [
+      "France","Allemange"
+    ],
+    "starthour" : 1720591200,
+    "endhour" : 1720596600,
+  },
+  {
+    "id" : 2,
+    "name" : "Match de Rugby",
+    "description" : "Match d'ouverture",
+    "sport" : "Rugby",
+    "place" : "Stade de A",
+    "versus" : [
+      "France","Allemange"
+    ],
+    "starthour" : 1720620000,
+    "endhour" : 1720627200
+  }
+  
+];
