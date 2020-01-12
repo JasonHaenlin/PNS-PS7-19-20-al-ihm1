53f36f132b6fbe4bb090ab28cab49db14d80e8c9
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-01-12 21:20:52 +0100

    #35 4 add search bar in view agenda

--- polympic/lib/screens/app-page/agenda-page/components/event_list.dart
+++ polympic/lib/screens/app-page/agenda-page/components/event_list.dart
@@ -4,34 +4,68 @@ import 'package:polympic/theme/colors.dart';
 
 import 'event_card.dart';
 
-class EventList extends StatelessWidget {
-  final List<EventModel> _data;
+class EventList extends StatefulWidget {
+  List<EventModel> _data;
+  List<EventModel> filteredData;
 
-  const EventList({
+  EventList({
     Key key,
     @required List<EventModel> data,
   })  : _data = data,
         super(key: key);
 
   @override
+  _EventListState createState() => _EventListState();
+}
+
+class _EventListState extends State<EventList> {
+
+  List<EventModel> filteredData = List();
+
+@override
+  void initState() {
+    super.initState();
+    filteredData = this.widget._data;
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Agenda"),
       ),
-      body: ListView.builder(
-        itemCount: _data.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: EventCard(event: _data[index]),
-              ),
-              Divider(thickness: 1.0, color: kColorIcon),
-            ],
-          );
-        },
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10.0),
+                prefixIcon: Icon(Icons.search),
+                hintText: "Enter an event name"),
+            onChanged: (filter) {
+              setState(() {
+                this.filteredData = this.widget._data
+                  .where((e) =>
+                      (e.name.toLowerCase().contains(filter.toLowerCase())))
+                  .toList();
+              });
+            },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredData.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: EventCard(event: this.widget._data[index]),
+                    ),
+                    Divider(thickness: 1.0, color: kColorIcon),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
