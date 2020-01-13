37e87fc99a58d59790ecbc352bd932802b6f7917
Florent ROBERT <florent.robert@etu.unice.fr>
2020-01-13 11:13:52 +0100

    #64 Function needed for tree and paths

--- polympic-server/controller/event.js
+++ polympic-server/controller/event.js
@@ -41,6 +41,37 @@ module.exports = {
       element.distance = geolib.getDistance(coord, coordElem);
     });
     return collection;
+  },
+
+  getEarliestEvent(events) {
+    let min = 23;
+    let minEvent;
+    events.forEach(event => {
+      let date = new Date(event.startTime*1000);
+      let hour = date.getHours();
+      if (hour < min) {
+        min = hour;
+        minEvent = event;
+      }
+      // if (events.startTime.)
+    });
+    return minEvent;
+  },
+
+  getNext(event, Nevents) {
+    let date = new Date(event.endTime*1000);
+    let hour = date.getHours();
+    let next = [];
+    console.log(hour);
+    Nevents.forEach(Nevent => {
+      let Ndate = new Date(Nevent.startTime*1000);
+      let Nhour = Ndate.getHours();
+      console.log(Nhour);
+      if ((Nhour - hour) <= 1 && (Nhour - hour) >= 0) {
+        next.push(Nevent.id);
+      }
+    });
+    return next;
   }
 
 };
--- polympic-server/tests/eventTest.js
+++ polympic-server/tests/eventTest.js
@@ -2,6 +2,70 @@ require('../bin/www');
 let assert = require('chai').assert;
 const { event } = require('../controller');
 
+Sevent =
+{
+  "id": "fdm2",
+  "name": "Football Demi Finale 1/2 ",
+  "description": "La demi finale présentant le match tant attendu Italie vs Espagne, lequel des deux ira en final ? !",
+  "versus": ["Italie","Espagne"],
+  "sport": "Football",
+  "site": {
+      "name" :"Stade de France A",
+      "latitude" :48.924459,
+      "longitude" :2.360164
+  },
+  "startTime": 1667721600,
+  "endTime": 1667725200
+}
+
+events = [
+  {
+    "id": "fdm2",
+    "name": "Football Demi Finale 1/2 ",
+    "description": "La demi finale présentant le match tant attendu Italie vs Espagne, lequel des deux ira en final ? !",
+    "versus": ["Italie","Espagne"],
+    "sport": "Football",
+    "site": {
+        "name" :"Stade de France A",
+        "latitude" :48.924459,
+        "longitude" :2.360164
+    },
+    "startTime": 1667721600,
+    "endTime": 1667725200
+},
+{
+    "id": "fdm3",
+    "name": "Football Demi Finale 2/2 ",
+    "description": "La demi finale présentant le match tant attendu France vs Allemagne, lequel des deux ira en final ? !",
+    "versus": ["France","Allemagne"],
+    "sport": "Football",
+    "site": {
+        "name" :"Stade de France B",
+        "latitude" :48.924459,
+        "longitude" :2.360164
+    },
+    "startTime": 1667728800,
+    "endTime": 1667736000
+},
+{
+  "id": "fdm4",
+  "name": "Football Demi Finale 2/2 ",
+  "description": "La demi finale présentant le match tant attendu France vs Allemagne, lequel des deux ira en final ? !",
+  "versus": ["France","Allemagne"],
+  "sport": "Football",
+  "site": {
+      "name" :"Stade de France B",
+      "latitude" :48.924459,
+      "longitude" :2.360164
+  },
+  "startTime": 1667736000,
+  "endTime": 1667736000
+}]
+
+// console.log(event.getNext(Sevent, events));
+
+// console.log(event.getEarliestEvent(events));
+
 // events = evenement.getEvenement();
 // coord1 = {latitude: 51.525, longitude: 7.4575};
 // console.log(evenement.getDistance(coord1, events));
