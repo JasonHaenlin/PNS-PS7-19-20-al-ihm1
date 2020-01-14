f169964389371e9a6a2a2b3fbaaf2ffef69fa15c
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-01-14 19:29:06 +0100

    #35 #4 update view agenda

--- polympic-server/controller/event.js
+++ polympic-server/controller/event.js
@@ -5,126 +5,133 @@ const geolib = require('geolib');
 const compiler = require('../utils/compiler');
 
 module.exports = {
-  getEvents() {
-    return this.filterByScript();
-  },
+    getEvents() {
+        return this.filterByScript();
+    },
 
-  // use the code in parameter
-  runScript(code) {
-    const events = Event.get();
-    const compiledScript = compiler.compileCode(code);
-    return eval(compiledScript).run(events);
-  },
+    // use the code in parameter
+    runScript(code) {
+        const events = Event.get();
+        const compiledScript = compiler.compileCode(code);
+        return eval(compiledScript).run(events);
+    },
 
-  // use the example script
-  filterByScript() {
-    const events = Event.get();
-    const scriptName = './public/scripts/example.js';
-    const compiledScript = compiler.compile(scriptName);
-    return eval(compiledScript).run(events);
-  },
+    // use the example script
+    filterByScript() {
+        const events = Event.get();
+        const scriptName = './public/scripts/example.js';
+        const compiledScript = compiler.compile(scriptName);
+        return eval(compiledScript).run(events);
+    },
 
-  getSpecificEvents(tags) {
-    const events = Event.get();
-    let empty = [];
-    tags.forEach(type => {
-      events.forEach(element => {
-        if (element.sport === type) {
-          empty.push(element);
+    getSpecificEvents(tags) {
+        const events = Event.get();
+        let empty = [];
+        tags.forEach(type => {
+            events.forEach(element => {
+                if (element.sport === type) {
+                    empty.push(element);
+                }
+            });
+        });
+        return empty;
+    },
+
+    getProximityEvents(tags) {
+        Eiffel_Tower_coord = { latitude: 48.858370, longitude: 2.294481 };
+        listEvents = this.getSpecificEvents(tags);
+        listEvents = this.getDistanceFromEvents(Eiffel_Tower_coord, listEvents);
+        return listEvents;
+    },
+
+    getSpecificListEvents(tags, events) {
+        let empty = [];
+        tags.forEach(type => {
+            events.forEach(element => {
+                if (element.sport === type) {
+                    empty.push(element);
+                }
+            });
+        });
+        return empty;
+    },
+
+    getSortedEvents(prop, asc) {
+        let events = Event.get();
+        return events.sort((a, b) => {
+            if (asc) {
+                return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+            } else {
+                return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+            }
+        });
+    },
+
+    getSortedListEvents(prop, asc, events) {
+        return events.sort((a, b) => {
+            if (asc) {
+                return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+            } else {
+                return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+            }
+        });
+    },
+
+    getDistanceFromEvents(coord, events) {
+        events.forEach(event => {
+            Vlatitude = event.site.latitude;
+            Vlongitude = event.site.longitude;
+            coordElem = { latitude: Vlatitude, longitude: Vlongitude };
+            event.distance = geolib.getDistance(coord, coordElem);
+        });
+        return events;
+    },
+
+    getEarliestEvent(events) {
+        let min = 23;
+        let minEvent;
+        events.forEach(event => {
+            let date = new Date(event.startTime * 1000);
+            let hour = date.getHours();
+            if (hour < min) {
+                min = hour;
+                minEvent = event;
+            }
+        });
+        return minEvent;
+    },
+
+    getNext(event, Nevents) {
+        let date = new Date(event.endTime * 1000);
+        let hour = date.getHours();
+        let next = [];
+        Nevents.forEach(Nevent => {
+            let Ndate = new Date(Nevent.startTime * 1000);
+            let Nhour = Ndate.getHours();
+            if ((Nhour - hour) <= 1 && (Nhour - hour) >= 0) {
+                next.push(Nevent.id);
+            }
+        });
+        return next;
+    },
+
+    getEventById(ids) {
+        let events = Event.get();
+        for (const e of events) {
+            if (e.id === ids) {
+                return e;
+            }
         }
-      });
-    });
-    return empty;
-  },
+        return 0;
+    },
 
-  getSpecificListEvents(tags, events) {
-    let empty = [];
-    tags.forEach(type => {
-      events.forEach(element => {
-        if (element.sport === type) {
-          empty.push(element);
+    getListEventsById(ids, events) {
+        for (const e of events) {
+            if (e.id === ids) {
+                return e;
+            }
         }
-      });
-    });
-    return empty;
-  },
-
-  getSortedEvents(prop, asc) {
-    let events = Event.get();
-    return events.sort((a, b) => {
-      if (asc) {
-        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
-      } else {
-        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
-      }
-    });
-  },
-
-  getSortedListEvents(prop, asc, events) {
-    return events.sort((a, b) => {
-      if (asc) {
-        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
-      } else {
-        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
-      }
-    });
-  },
-
-  getDistanceFromEvents(coord, events) {
-    events.forEach(event => {
-      Vlatitude = event.site.latitude;
-      Vlongitude = event.site.longitude;
-      coordElem = { latitude: Vlatitude, longitude: Vlongitude };
-      event.distance = geolib.getDistance(coord, coordElem);
-    });
-    return events;
-  },
-
-  getEarliestEvent(events) {
-    let min = 23;
-    let minEvent;
-    events.forEach(event => {
-      let date = new Date(event.startTime * 1000);
-      let hour = date.getHours();
-      if (hour < min) {
-        min = hour;
-        minEvent = event;
-      }
-    });
-    return minEvent;
-  },
-
-  getNext(event, Nevents) {
-    let date = new Date(event.endTime * 1000);
-    let hour = date.getHours();
-    let next = [];
-    Nevents.forEach(Nevent => {
-      let Ndate = new Date(Nevent.startTime * 1000);
-      let Nhour = Ndate.getHours();
-      if ((Nhour - hour) <= 1 && (Nhour - hour) >= 0) {
-        next.push(Nevent.id);
-      }
-    });
-    return next;
-  },
-
-  getEventById(ids) {
-    let events = Event.get();
-    for (const e of events) {
-      if (e.id === ids) {
-        return e;
-      }
+        return 0;
     }
-    return 0;
-  },
-
-  getListEventsById(ids, events) {
-    for (const e of events) {
-      if (e.id === ids) {
-        return e;
-      }
-    }
-    return 0;
-  }
 
 };
