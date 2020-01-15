7bcbeebe3db48ea951cbd6410162f7856ef3012d
PaulmarieDjekinnou <pmdjek@gmail.com>
2020-01-15 09:22:18 +0100

    #11 fix

--- polympic-server/routes/events/event.js
+++ polympic-server/routes/events/event.js
@@ -1,18 +1,25 @@
 const resHandler = require('../../utils/response-handler');
 const { event } = require('../../controller');
 
-exports.getEventsByPreferences = async (req, res) => {
-  let prefs = req.query.prefs;
-  let events;
-  if (prefs) {
-    events = event.getProximityEvents(prefs.split(','));
-  } else {
-    events = event.getEvents();
-  }
-  resHandler.yahResponse(res, events);
+exports.getEventsByPreferences = async(req, res) => {
+    let prefs = req.query.prefs;
+    let events;
+    if (prefs) {
+        events = event.getProximityEvents(prefs.split(','));
+        events.forEach(element => {
+            if (element.listNearEvents) {
+                delete element.listNearEvents;
+            }
+        });
+        console.log(events);
+
+    } else {
+        events = event.getEvents();
+    }
+    resHandler.yahResponse(res, events);
 };
 
-exports.previewResult = async (req, res) => {
-  let code = req.body.code;
-  resHandler.yahResponse(res, event.runScript(code));
+exports.previewResult = async(req, res) => {
+    let code = req.body.code;
+    resHandler.yahResponse(res, event.runScript(code));
 };
