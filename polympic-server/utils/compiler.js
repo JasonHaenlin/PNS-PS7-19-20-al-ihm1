f6e79fe50beb82f107e296f24d7ad1192d06f9a7
Florent ROBERT <florent.robert@etu.unice.fr>
2020-01-19 22:09:19 +0100

    revert dsl bug comment

--- polympic-server/utils/compiler.js
+++ polympic-server/utils/compiler.js
@@ -4,9 +4,9 @@ const cp = require('child_process');
 
 // compile a text and return some JS code
 const compileCode = (code) => {
- //  const response = cp.execSync('echo "' + code.replace('\n', ' ').replace('\r', ' ') +
- //       '" | java -jar ./utils/compiler/polympic-dsl.jar').toLocaleString();
- //  return response;
+  const response = cp.execSync('echo "' + code.replace('\n', ' ').replace('\r', ' ') +
+        '" | java -jar ./utils/compiler/polympic-dsl.jar').toLocaleString();
+  return response;
 };
 
 
