0d6e3714319fa73f624ebd10ad99233cf0e7d53b
Florent ROBERT <florent.robert@etu.unice.fr>
2020-01-19 20:20:27 +0100

    #85 comment route resto

--- polympic-server/routes/restaurant/index.js
+++ polympic-server/routes/restaurant/index.js
@@ -6,22 +6,24 @@ const r = require('./restaurant');
 const restaurant = express.Router();
 
 /**
- * @api {get} /preferences Request a list of preferences
- * @apiName GetPreferences
- * @apiGroup Preferences
- * @apiParam {string} select query params to get the preferences filtered.
- * Should be a list of preferences separeted by comma ','
+ * @api {get} /restaurant Request a list of restaurant
+ * @apiName restaurantsWithDistance
+ * @apiGroup restaurant
+ * @apiParam {string} select latitude longitude to get distance with restaurant
  * @apiExample {curl} Example usage:
- * curl -i http://localhost:3000/preferences?select=sport
+ * curl -i http://localhost:3000/restaurant?select={latitude: 48.922456, longitude: 2.361977}'
  * @apiSucess (200) {json[]} events list
  * @apiExample Response:
  * HTTP/1.1 200 OK
  * [
- *      {
- *          "name": "Football",
- *          "tag": "sport",
- *          "img": ""
- *      }
+    {
+        "name" : "Sushi Shop",
+        "position" : {
+            "latitude" :48.919541,
+            "longitude" :2.362471
+        }
+        "distance" : 11111
+    }
  * ]
  */
 restaurant.get('/', handleExceptions(r.restaurantsWithDistance));
