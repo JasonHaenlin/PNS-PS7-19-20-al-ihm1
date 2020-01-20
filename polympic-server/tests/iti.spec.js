9162adbe278f55edf782c4ece9776b55237c3fc9
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-01-20 21:20:46 +0100

    GH-74 update the back like the front model on event

--- polympic-server/tests/iti.spec.js
+++ polympic-server/tests/iti.spec.js
@@ -4,5 +4,4 @@ const Itinerary = require('../app/controller/itinerary');
 const Events = require('../app/controller/event');
 
 let iti = Itinerary.generateItinerary();
-console.log(iti);
-
+// console.log(iti);
