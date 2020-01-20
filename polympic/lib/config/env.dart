68f9554a13ff173c136b9ec410b7882987b84943
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-01-20 21:58:19 +0100

    GH-102 add the env file to launch the app in the server

---
+++ polympic/lib/config/env.dart
@@ -0,0 +1,7 @@
+const kEnvAppName = 'Polympic';
+
+const kEnvDev = 'dev';
+const kEnvProd = 'prod';
+
+const kEnvApiUrlDev = '';
+const kEnvApiUrlProd = 'https://polympic.otakedev.com/';
--- polympic/lib/config/env_example.dart
+++
@@ -1,7 +0,0 @@
-const kEnvAppName = 'MyApp';
-
-const kEnvDev = 'dev';
-const kEnvProd = 'prod';
-
-const kEnvApiUrlDev = '';
-const kEnvApiUrlProd = '';
