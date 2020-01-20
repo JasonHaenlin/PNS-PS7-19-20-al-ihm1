34d7e283fe071d5419b26df8cabdbe56c585e0df
Florent ROBERT <florent.robert@etu.unice.fr>
2020-01-20 16:04:46 +0100

    #87 Restaurant + Itinerary

--- polympic-server/app/controller/event.js
+++ polympic-server/app/controller/event.js
@@ -11,80 +11,80 @@ const getHourfromDate = (timestamp) => new Date(timestamp * 1000).getHours();
 
 
 module.exports = {
-    getEvents() {
-        return this.filterByScript();
-    },
+  getEvents() {
+    return this.filterByScript();
+  },
 
-    // use the code in parameter
-    runScript(code) {
-        let coords = {latitude: 48.858370, longitude: 2.294481};
-        let events = this.measureDistance(coords, Event.get());
-        const compiledScript = compiler.compileCode(code);
-        return eval(compiledScript).run(events);
-    },
+  // use the code in parameter
+  runScript(code) {
+    let coords = {latitude: 48.858370, longitude: 2.294481};
+    let events = this.measureDistance(coords, Event.get());
+    const compiledScript = compiler.compileCode(code);
+    return eval(compiledScript).run(events);
+  },
 
-    // use the example script
-    filterByScript() {
-        let coords = {latitude: 48.858370, longitude: 2.294481};
-        let events = this.measureDistance(coords, Event.get());
-        const scriptName = './app/public/scripts/example.script';
-        const compiledScript = compiler.compile(scriptName);
-        let result;
-        try {
-            // required for the compiled script
-            const lib = require('../utils/compiler/lib');
-            result = eval(compiledScript).run(events);
-        } catch (e) {
-            result = [];
-        }
-        return result;
-    },
+  // use the example script
+  filterByScript() {
+    let coords = {latitude: 48.858370, longitude: 2.294481};
+    let events = this.measureDistance(coords, Event.get());
+    const scriptName = './app/public/scripts/example.script';
+    const compiledScript = compiler.compile(scriptName);
+    let result;
+    try {
+      // required for the compiled script
+      const lib = require('../utils/compiler/lib');
+      result = eval(compiledScript).run(events);
+    } catch (e) {
+      result = [];
+    }
+    return result;
+  },
 
-    filterEventsByTags(tags, events) {
-        events = events || Event.get();
-        return events.filter((elem) => tags.includes(elem.sport));
-    },
+  filterEventsByTags(tags, events) {
+    events = events || Event.get();
+    return events.filter((elem) => tags.includes(elem.sport));
+  },
 
-    filterEventsByProximityAndTags(tags, from) {
-        let events = this.getEvents();
-        from = from || EIFFEL_TOWER_COORD;
-        events = this.filterEventsByTags(tags, events);
-        events = this.measureDistance(from, events);
-        return events;
-    },
+  filterEventsByProximityAndTags(tags, from) {
+    let events = this.getEvents();
+    from = from || EIFFEL_TOWER_COORD;
+    events = this.filterEventsByTags(tags, events);
+    events = this.measureDistance(from, events);
+    return events;
+  },
 
-    sortEvents(prop, asc, events) {
-        events = events || Event.get();
-        return events.sort((a, b) => {
-            if (asc) {
-                return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
-            } else {
-                return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
-            }
-        });
-    },
+  sortEvents(prop, asc, events) {
+    events = events || Event.get();
+    return events.sort((a, b) => {
+      if (asc) {
+        return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+      } else {
+        return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+      }
+    });
+  },
 
-    /**
+  /**
      * get the distance between two evetns
      * @param {GeolibInputCoordinates} coord : from this localisation
      * @param {GeolibInputCoordinates} events : to this localisation
      */
-    measureDistance(coord, events) {
-        events.forEach(event => {
-            Vlatitude = event.site.latitude;
-            Vlongitude = event.site.longitude;
-            coordElem = {latitude: Vlatitude, longitude: Vlongitude};
-            let distance = geolib.getDistance(coord, coordElem);
-            event.distance = distance;
-        });
-        return events;
-    },
+  measureDistance(coord, events) {
+    events.forEach(event => {
+      Vlatitude = event.site.latitude;
+      Vlongitude = event.site.longitude;
+      coordElem = {latitude: Vlatitude, longitude: Vlongitude};
+      let distance = geolib.getDistance(coord, coordElem);
+      event.distance = distance;
+    });
+    return events;
+  },
 
-    retrieveEarliestEvent(events) {
-        return events.reduce((min, e) => e.startTime < min.startTime ? e : min, events[0]);
-    },
+  retrieveEarliestEvent(events) {
+    return events.reduce((min, e) => e.startTime < min.startTime ? e : min, events[0]);
+  },
 
-    /**
+  /**
      * Retrives the events that start not too late and not too soon
      * after the selected event
      * @param {*} event the parent event
@@ -92,18 +92,22 @@ module.exports = {
      * @param {*} gapMin the min time in minutes (min)
      * @param {*} gapMax the max time in minutes (min)
      */
-    retrieveEventWithinGapHour(event, events, gapMin, gapMax) {
-        gapMin = gapMin || 0;
-        gapMax = gapMax || 1;
-        return _.filter(events, (e) => {
-            return _.inRange((getHourfromDate(e.startTime) - getHourfromDate(event.endTime)),
-                gapMin, gapMax + 0.1);
-        });
-    },
+  retrieveEventWithinGapHour(event, events, gapMin, gapMax) {
+    gapMin = gapMin || 0;
+    gapMax = gapMax || 1;
+    return _.filter(events, (e) => {
+      return _.inRange((getHourfromDate(e.startTime) - getHourfromDate(event.endTime)),
+        gapMin, gapMax + 0.1);
+    });
+  },
 
-    getEventById(ids, events) {
-        events = events || Event.get();
-        return _.find(events, (e) => e.id === ids);
-    }
+  retrieveEventWithSameHour(event, events) {
+    return _.filter(events, (e) => getHourfromDate(e.startTime) === getHourfromDate(event.startTime));
+  },
+
+  getEventById(ids, events) {
+    events = events || Event.get();
+    return _.find(events, (e) => e.id === ids);
+  }
 
 };
